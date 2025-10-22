c6f75cbb85ee75e93146057f3f8a2da4ee4832a6
Khoa Tran <79559262+kchan139@users.noreply.github.com>
2025-10-22 09:54:12 +0700

    update eza alias

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,22 +1,22 @@
 IGNORE='node_modules|.git|dist|build|target|.next|venv|__pycache__|*.log|.DS_Store'
 
-alias l='eza -1s type --group --icons --git'
-alias la='eza -as type --group --icons --git'
-alias ll='eza -las type --group --icons --git'
-alias ls='eza -F --group-directories-first --group --icons --git'
-alias llz='eza -Zlas type --octal-permissions --group --icons --git'
+alias ls='eza --group-directories-first --group --icons --git'
+alias l='eza -1s type --group-directories-first --group --icons --git'
+alias la='eza -F -as type --group-directories-first --group --icons --git'
+alias ll='eza -las type --group-directories-first --group --icons --git'
 alias ltr='eza -T --level=2 --group-directories-first --group --icons --git'
+alias llz='eza -Zlas type --group-directories-first --octal-permissions --group --icons --git'
 
 llr() {
   eza -RZlas type --group --icons --git -I "$IGNORE" "$@"
 }
 
 tree() {
-  eza --icons -aTI "$IGNORE" "$@"
+  eza --icons --group-directories-first -aTI "$IGNORE" "$@"
 }
 
 jsontree() {
-  eza --icons -aTI "$IGNORE" -J "$@" | jq '.'
+  \tree --group-directories-first -aTI "$IGNORE" -J "$@" | jq '.'
 }
 
 alias tf='tofu'
