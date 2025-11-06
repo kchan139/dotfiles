df6abc83fcce59a00b2eb7d790fdff71d6dd8f99
Khoa Tran <ovn6002@gmail.com>
2025-11-06 21:59:24 +0700

    update brew package, zsh/zed config

--- config/zsh/tmux.zsh
+++ config/zsh/tmux.zsh
@@ -1,4 +1,6 @@
 # Attach or create tmux session automatically
-if command -v tmux >/dev/null 2>&1 && [ -z "$TMUX" ]; then
+case $- in *i*) interactive=1 ;; *) interactive=0 ;; esac
+
+if command -v tmux >/dev/null 2>&1 && [ "$interactive" -eq 1 ] && [ -z "$TMUX" ] && [ -z "$ZED_TERMINAL" ] && [ -z "$ZED_TERM" ] && [ "$TERM_PROGRAM" != "zed" ]; then
   tmux attach || tmux new
 fi
