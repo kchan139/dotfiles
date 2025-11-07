31f1e831ef06a39ef2d6ba3a16d76ee3a19c87a5
Khoa Tran <ovn6002@gmail.com>
2025-11-07 23:24:03 +0700

    disable podmancompose warning

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -1,8 +1,11 @@
 # environment variables & paths
-export PATH="$HOME/.local/bin:$PATH"
 export EDITOR=hx
 export VISUAL=hx
+
 export EZA_CONFIG_DIR="$HOME/.config/eza"
+export PODMAN_COMPOSE_WARNING_LOGS=false
+
+export PATH="$HOME/.local/bin:$PATH"
 
 if [ -d "/usr/local/go/bin" ]; then
   export PATH="$PATH:/usr/local/go/bin"
